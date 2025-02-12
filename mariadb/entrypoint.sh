a5f5876ebb77b19993b45f4d8f00d3782cb6b060
seojilee <seojilee@student.42seoul.kr>
2025-02-12 10:15:11 +0900

    add a rough draft of mariadb configs

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-apk --update add mariadb #mariadb-client
-chown -R mysql:mysql /var/lib/mysql
-mysql_install_db --user=mysql #--datadir=/var/lib/mysql
-mkdir /run/mysqld
-chown -R mysql:mysql /run/mysqld
-mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock
+mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock &
+
+while ! mysqladmin ping -h localhost --silent; do
+    sleep 1
+done
+
+mysql -u root <<EOF
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+EOF
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+CREATE DATABASE $MYSQL_DATABASE;
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+
+wait
